d61b329aa57c7c817335db4fa905d371727fb8f1
nigraforce <aleshka2012@gmail.com>
2019-09-04 13:29:39 +0300

    added JSDoc style comment

--- source/zip.js
+++ source/zip.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/**
+ * Merges objects into one object 
+ * @param {...object} args Objects to merge
+ */
 const zip = function (...args) {
     return Object.assign({}, ...Array.from(args).reverse())
 }
