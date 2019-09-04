2bccd96ebd3a7f1d9a17e21d165233f2c110e677
nigraforce <aleshka2012@gmail.com>
2019-09-04 18:13:38 +0300

    remade zip so that it zips objects regardless of depth

--- source/zip.js
+++ source/zip.js
@@ -5,5 +5,35 @@
  * @param {...object} args Objects to merge
  */
 const zip = (...args) => {
-    return Object.assign({}, ...Array.from(args).reverse())
+    // чтобы не перезаписывать первое встретившееся поле
+    args = args.reverse()
+
+    var subobjects = {}
+
+    // ищем объекты которые надо объединить
+    for (var arg of args) {
+        if (arg) {
+            for (var prop_name of Object.keys(arg)) {
+                if (typeof arg[prop_name] === 'object') {
+                    if (!subobjects[prop_name]) {
+                        subobjects[prop_name] = []
+                    }
+                    subobjects[prop_name].push(arg[prop_name])
+                }
+            }
+        } else {
+            throw TypeError('undefined argument')
+        }
+    }
+
+    // рекурсивно сливаем объекты в иерархии основного объекта
+    for (var idx of Object.keys(subobjects)) {
+        if (subobjects[idx].length > 1) {
+            subobjects[idx] = zip(...subobjects[idx])
+        } else {
+            subobjects[idx] = subobjects[idx][0]
+        }
+    }
+
+    return Object.assign({}, ...args, subobjects)
 }
