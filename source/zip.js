0a837e09f5176779b07cbd0563cada9b1ed1e56c
nigraforce <aleshka2012@gmail.com>
2019-09-04 13:38:27 +0300

    added lambda function syntax

--- source/zip.js
+++ source/zip.js
@@ -4,6 +4,6 @@
  * Merges objects into one object 
  * @param {...object} args Objects to merge
  */
-const zip = function (...args) {
+const zip = (...args) => {
     return Object.assign({}, ...Array.from(args).reverse())
 }
